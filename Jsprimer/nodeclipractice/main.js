8dea4194a9b3104d4e89eb9c02a888b6cf364334
kei <keinick11@outlook.com>
2022-02-03 16:29:15 +0900

    Node CLI Practice

---
+++ Jsprimer/nodeclipractice/main.js
@@ -0,0 +1,29 @@
+console.log(process.argv);
+// my-module.jsモジュールをmyModuleオブジェクトとしてインポートする。
+const myModule = require("./my-module");
+console.log(myModule.foo); // => "foo"
+
+const program = require("commander");
+const fs = require("fs");
+// md2htmlモジュールをインポートする
+const md2html = require("./md2html");
+
+program.option("--gfm", "GFMを有効にする");
+program.parse(process.argv);
+const filePath = program.args[0];
+
+const cliOptions = {
+    gfm: false,
+    ...program.opts(),
+};
+
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+        return;
+    }
+    // md2htmlモジュールを使ってHTMLに変換する
+    const html = md2html(file, cliOptions);
+    console.log(html);
+});
