66c4d22063ae104a8d745e052d4f7990d1a04e4e
kei <keinick11@outlook.com>
2022-02-06 01:46:04 +0900

    Function Not to Add Todo If Blank

--- Jsprimer/todo/src/App.js
+++ Jsprimer/todo/src/App.js
@@ -55,8 +55,11 @@ export class App {
 
         formElement.addEventListener("submit", (event) => {
             event.preventDefault();
-            this.handleAdd(inputElement.value);
-            inputElement.value = "";
+            if(inputElement.value!==""){
+              this.handleAdd(inputElement.value);
+              inputElement.value = "";
+            }
+
         });
     }
 }
