9f62c97da087fcf6c5f1e3aa38597e23c16fbc0a
kei <keinick11@outlook.com>
2022-02-20 15:06:04 +0900

    Make class

---
+++ Recursion/Clicker Empire Game/index.js
@@ -0,0 +1,11 @@
+class User{
+  constructor(name,old,money,days,assets){
+    this.name = name ;
+    this.old = old ;
+    this.money = money ;
+    this.days = days ;
+    this.assets = assets ;
+  }
+
+}
+
