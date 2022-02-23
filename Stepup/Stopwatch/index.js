45880c1898dc990490dfa3c3f4df9f7cf4ace87b
kei <pipin0715@gmail.com>
2022-02-23 23:04:37 +0900

    refacturring getelement

--- Stepup/Stopwatch/index.js
+++ Stepup/Stopwatch/index.js
@@ -1,8 +1,8 @@
 function countHelper(){
 
-  let start = document.querySelector("#start");
-  let end = document.querySelector("#stop");
-  let display = document.querySelector("#display")
+  let start = document.getElementById("start");
+  let end = document.getElementById("stop");
+  let display = document.getElementById("display")
   let cnt = 0 ;
 
   let num ;
@@ -29,7 +29,7 @@ function countHelper(){
 function getTime(str){
   const date1 = new Date() ;
   const date2 =  date1.getHours() + "時" + date1.getMinutes() + "分" + date1.getSeconds() + "秒" + " " + str ;
-  let lists = document.querySelector("#lists");
+  let lists = document.getElementById("lists");
   let list = document.createElement("div") ;
   list.classList.add("col-12", "border-bottom", "text-center", "border-light");    
 
