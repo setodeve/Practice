72759cc507a0b47a5460d9b117dc9fd65d4e1862
setodeve <keinick11@outlook.com>
2022-03-22 18:20:29 +0900

    Finish user edit, update, index, and destroy actions

--- RailsTutorial/sample_app/db/schema.rb
+++ RailsTutorial/sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_234954) do
+ActiveRecord::Schema.define(version: 2022_03_22_084154) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_03_21_234954) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
