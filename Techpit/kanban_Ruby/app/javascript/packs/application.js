dd8ef4d1e696f1d539e07b687a965744f3e11fb3
setodeve <keinick11@outlook.com>
2022-03-28 19:33:15 +0900

    Add Bootstrap

--- Techpit/kanban_Ruby/app/javascript/packs/application.js
+++ Techpit/kanban_Ruby/app/javascript/packs/application.js
@@ -1,3 +1,4 @@
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -8,7 +9,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
+import 'bootstrap';
+import '../stylesheets/application';
+import '@fortawesome/fontawesome-free/js/all';
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
