aad27e559d0c738bd8b52e7570480f1bb77d2eba
setodeve <keinick11@outlook.com>
2022-04-02 23:00:51 +0900

    Like and Comment

--- Techpit/techpitgram_Ruby/config/routes.rb
+++ Techpit/techpitgram_Ruby/config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   devise_for :users, 
     controllers: { registrations: 'registrations' } 
   
-  root 'pages#home'
+  root 'posts#index'
+
   get '/users/:id', to: 'users#show', as: 'user'
+
+  resources :posts, only: %i(index show new create destroy) do
+    resources :photos, only: %i(create)
+    resources :likes, only: %i(create destroy)
+    resources :comments, only: %i(create destroy)
+  end
 end
