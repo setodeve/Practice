9f69ad479ec58ae65e7c5c28b37e0f3eb22082e4
setodeve <keinick11@outlook.com>
2022-04-08 22:54:30 +0900

    Rspec practice for sampleapp

--- Techpit/rspec-sample/config/application.rb
+++ Techpit/rspec-sample/config/application.rb
@@ -26,7 +26,9 @@ module RspecSample
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
