b9963b1f57b1555d752e2c92fb35d54835490063
setodeve <keinick11@outlook.com>
2022-04-11 21:19:58 +0900

    First Commit Tuning

---
+++ Techpit/rails_tuning_sample_app/app/controllers/profiles_controller.rb
@@ -0,0 +1,21 @@
+class ProfilesController < ApplicationController
+   def index
+    @user = User.find(1)
+    raise Forbidden unless user_safe?
+
+    @skill_categories = user_reccomend_skill_categories
+    @articles = @user.articles
+  end
+
+  private
+
+  def user_safe?
+    @user.user_cautions.all? do |user_caution|
+      Time.zone.now > user_caution.caution_freeze.end_time
+    end
+  end
+
+  def user_reccomend_skill_categories
+    @user.skills.map(&:skill_category).filter { |skill_category| skill_category.reccomend }.uniq
+  end
+end
