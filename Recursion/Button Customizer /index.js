8773f9ee08717b80d1214a6f9a0e5451853b639f
setodeve <keinick11@outlook.com>
2022-04-24 21:56:38 +0900

    Vue Practice

---
+++ Recursion/Button Customizer /index.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Button Customizer',
+    activeColor: '#000000',
+    backColor: '#ffffff',
+    fontSize: 30,
+    fontOpacity: 1,
+    borderRadius: 50,
+    borderSet: 'solid'
+  }
+})
