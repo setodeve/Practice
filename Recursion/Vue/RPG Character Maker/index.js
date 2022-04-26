fa32c0c8c69d08d9060966e6c4dbe66143e5d6b8
setodeve <keinick11@outlook.com>
2022-04-26 14:38:09 +0900

    RPG Character Maker

--- Recursion/Vue/RPG Character Maker/index.js
+++ Recursion/Vue/RPG Character Maker/index.js
@@ -16,7 +16,9 @@ const CaractersInfo ={
 
 const Gender = ["Male","Female"] ;
 
-console.log(CaractersInfo['Hero'][0])
+const skills = ["Everyman","Bat out of hell","Brave","Lucky devil","Tomboy"]
+
+// console.log(CaractersInfo['Hero'][0])
 
 new Vue({
   el: '#charSetting',
@@ -25,9 +27,57 @@ new Vue({
       name: 'Main',
       jobName: 'Hero',
       gender: '0',
-      traits: 'Brave',
-      charImage: 'https://recursionist.io/img/dashboard/lessons/quickstart/male-mage.png'
+      traits: '0',
+      charImage: 'https://recursionist.io/img/dashboard/lessons/quickstart/male-mage.png',
+      Strength: 10,
+      Agility: 10,
+      Resilience: 10,
+      Wisdom: 10,
+      Luck: 10,
     };
+  },
+  methods: {
+    initSkill(){
+      this.Strength = 10 ;
+      this.Agility = 10 ;
+      this.Resilience = 10 ;
+      this.Wisdom = 10 ;
+      this.Luck = 10 ;
+    },
+    initTraits(){
+      this.traits = '0'
+    },
+    changeJob(){
+      skill = this._data.traits ;
+      job = this._data.jobName ;
+      if((job!='Hero') && (skill=='2')){
+        console.log('ttete');
+        this.initTraits() ;
+        return ;
+      }
+    },
+    changeSkill(){
+      this.initSkill() ;
+      skill = this._data.traits ;
+      job = this._data.jobName ;
+      // console.log(skill) ;
+      if(skill==='1'){
+      //bat out of hell
+        this.Agility *= 1.40 ;
+      }else if(skill==='2'){
+      //brave
+        this.Strength *= 1.10 ;
+        this.Agility *= 1.10 ;
+        this.Luck *= 1.20 ;
+      }else if(skill==='3'){
+      //lucky devil
+        this.Luck *= 1.50 ;
+      }else if(skill==='4'){
+      //tomboy
+        this.Strength *= 1.10 ;
+        this.Agility *= 1.10 ;
+      }
+    },
   },
   computed: {
     checkImage(){
@@ -39,6 +89,11 @@ new Vue({
       return (num)=>{
         return Gender[num];
       }
-    }
+    },
+    checkskill(){
+      return (num)=>{
+        return skills[num];
+      }
+    },
   }
 })
