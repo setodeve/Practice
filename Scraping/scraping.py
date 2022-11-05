3518268c42104ec3ce480e8668cbe1996b8fab6c
setodeve <keinick11@outlook.com>
2022-11-05 22:31:10 +0900

    Correct garbled characters

--- Scraping/scraping.py
+++ Scraping/scraping.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "URL指定"
+url = "url"
 r = requests.get(url)
 
 getData = BeautifulSoup(r.content, "html.parser")
 
-results= getData.select("クラス、id指定") 
+results= getData.select("class and id") 
 
 
 for result in results:
-    # 改行+タブ(エクセルなどのテーブルにセルごとに貼り付けたい場合)
+    #new line + tab in case of pasting for excel
     print(result.get_text() + "\n\t" )
 
-    # 改行のみ(文書に貼り付けたい場合)
+    #new line in case of pasting for document
     print(result.get_text() + "\n" )
 
