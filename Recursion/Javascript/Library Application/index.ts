15f1d22edeede788734dc1d860ba290060a39fdc
setodeve <keinick11@outlook.com>
2022-12-17 16:17:49 +0900

    add event

--- Recursion/Javascript/Library Application/index.ts
+++ Recursion/Javascript/Library Application/index.ts
@@ -2,4 +2,10 @@ function test(data){
   console.log(data);
 }
 
+function getInput(){
+  console.log(document.getElementById("search-input").value);
+}
+
+const searchbutton = document.getElementById("basic-text1")
+searchbutton.addEventListener("click",getInput);
 test(1);
