95c4d65c8297f7c47f7963b76071274843c528a6
setodeve <keinick11@outlook.com>
2022-12-17 17:03:53 +0900

    add html function

--- Recursion/Javascript/Library Application/index.ts
+++ Recursion/Javascript/Library Application/index.ts
@@ -6,7 +6,63 @@ function searchInput(){
   const input = document.getElementById("search-input").value;
   fetch(config.url + input)
   .then(jsondata => jsondata.json())
-  .then(data => console.log(data))
+  .then(data => generateBookCard(data["ISBN:0451526538"]))
+}
+
+function arrayJoin(array){
+  let rtnString = ""
+
+  for(let i=0;i<array.length-1;i++){
+    rtnString += array[i].name + ", "
+  }
+  return rtnString + array[array.length-1].name;
+}
+
+function generateBookCard(data){
+console.log(data)
+const parent = document.getElementById('result');
+const cardTemplate = `
+    <div class="pb-3 pt-4">
+    <p class="m-0">results</p>
+    </div>
+    <div class="row no-gutters">
+    <div class="col-md-4">
+        <img src="${data.cover.small}" class="card-img p-3" alt="">
+    </div>
+    <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title m-0 font-weight-bold">${data.title}</h5>
+            <p class="m-0">${data.authors[0].name}</p>
+            <p class="m-0 pt-2">$12.11</p>
+            <p class="card-text pt-2 book-description">${data.by_statement}</p>
+        </div>
+    </div>
+    </div>
+    <div class="col-10">
+    <table class="table table-striped">
+        <tbody>
+        <tr>
+            <th scope="row">Page</th>
+            <td>${data.number_of_pages}</td>
+        </tr>
+        <tr>
+            <th scope="row">Publisher</th>
+            <td>${data.publishers[0].name}</td>
+        </tr>
+        <tr>
+            <th scope="row">Published Date</th>
+            <td>${data.publish_date}</td>
+        </tr>
+        <tr>
+            <th scope="row">Categories</th>
+            <td>${arrayJoin(data.subjects)}</td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+
+`
+parent.innerHTML = cardTemplate;
 }
 
 const searchButton = document.getElementById("basic-text1")
