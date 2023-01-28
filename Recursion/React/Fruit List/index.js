c911b00772571e34985d76043a575109f935e824
setodeve <keinick11@outlook.com>
2023-01-28 14:33:45 +0900

    Split file

--- Recursion/React/Fruit List/index.js
+++ Recursion/React/Fruit List/index.js
@@ -1,13 +1,2 @@
 let dom = document.getElementById("root");
-const { useEffect } = React;
-const App = () => {
-    useEffect(() => {
-            console.log("Processed useEffect");
-        })
-    return(
-        <div>
-            <p> CDN only で Reactが使えるか確認中 </p>
-        </div>
-    );
-}
-ReactDOM.render(<App />, dom);
+dom.innerHTML = "test";
