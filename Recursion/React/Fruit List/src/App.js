606302744439ad9579c1facfab7d7dff2928c188
setodeve <keinick11@outlook.com>
2023-01-29 19:12:55 +0900

    Fruits list react app

--- Recursion/React/Fruit List/src/App.js
+++ Recursion/React/Fruit List/src/App.js
@@ -4,20 +4,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="Fruit">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>Orange</h3>
+          <p>Calorie : **kcal</p>
+          <p>Macro : **g **g **g</p>
+        </div>
+        <div className="Fruit">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>Orange</h3>
+          <p>Calorie : **kcal</p>
+          <p>Macro : **g **g **g</p>
+        </div>
+        <div className="Fruit">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>Orange</h3>
+          <p>Calorie : **kcal</p>
+          <p>Macro : **g **g **g</p>
+        </div>
+        <div className="Fruit">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>Orange</h3>
+          <p>Calorie : **kcal</p>
+          <p>Macro : **g **g **g</p>
+        </div>
+        
     </div>
   );
 }
