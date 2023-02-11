6925865d447e24319e578bb99b91ef23f3dee947
setodeve <keinick11@outlook.com>
2023-02-11 18:36:32 +0900

    Add comment

--- work/minutes-management/test.py
+++ work/minutes-management/test.py
@@ -10,11 +10,11 @@ lists = {}
 for i in files: #file読み込み
   file = openpyxl.load_workbook(i)
   ws = file[SHEET]
-  while(1):
-    if(ws.cell(row_num, 2).value is None): break
-    if((ws.cell(row_num, 3).value is None)):
+  while(1): #各行読み出し
+    if(ws.cell(row_num, 2).value is None): break # 指摘が空だったら
+    if((ws.cell(row_num, 3).value is None)): # 指摘修正が空だったら
      lists[i] = "未完了"
      break
     row_num += 1
   
-print(lists)
+print(lists) #指摘が未完了の議事録だけ表示
