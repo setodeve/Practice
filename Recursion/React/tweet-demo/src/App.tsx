350130dd666e00f74b12e2b296f2a32a72a6e24a
setodeve <keinick11@outlook.com>
2023-02-12 19:20:21 +0900

    Add Form component

--- Recursion/React/tweet-demo/src/App.tsx
+++ Recursion/React/tweet-demo/src/App.tsx
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import dog from './dog.jpg';
+import bird from './bird.jpg';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
 }
 
-export default App;
+export function Form() {
+  const [name, setName] = useState('') ;
+  const [id, setId] = useState('') ;
+  const [textarea, setText] = useState('') ;
+  const [image, setImage] = useState(dog) ;
+
+  return (
+    <div className='App'>
+        <label>
+        name:
+        <input
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        id:
+        <input
+          value={id}
+          onChange={e => setId(e.target.value)}
+        />
+      </label>
+      <label>
+        textarea:
+        <input
+          value={textarea}
+          onChange={e => setText(e.target.value)}
+        />
+      </label>
+      <label >
+        image:
+        <select value={image} onChange={e => setImage(e.target.value)}>
+          <option value={dog}>dog</option>
+          <option value={bird}>bird</option>
+        </select>
+      </label>
+      {name !== '' &&
+        <p>{name}</p>
+      }
+      {id !== '' &&
+        <p>{id}</p>
+      }
+      {textarea !== '' &&
+        <p>{textarea}</p>
+      }
+      {
+        <img src={image} alt="test" />
+      }
+    </div>
+  );
+}
