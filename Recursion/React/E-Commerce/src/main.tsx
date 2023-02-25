d6582d42d49abad1f4e609a0541554da97f09982
setodeve <keinick11@outlook.com>
2023-02-25 21:40:32 +0900

    Add products

--- Recursion/React/E-Commerce/src/main.tsx
+++ Recursion/React/E-Commerce/src/main.tsx
@@ -7,10 +7,31 @@ import {
 import "./index.css";
 import Root from "./routes/root"
 
+const products = [
+  {id:1, tag:"Mac", name:"Macbook Air", price:115280},
+  {id:2, tag:"Mac", name:"Macbook Pro 13inch", price:148280},
+  {id:3, tag:"Mac", name:"Macbook Pro 14inch", price:239800},
+  {id:4, tag:"iPhone", name:"iPhone 13Pro", price:122800},
+  {id:5, tag:"iPhone", name:"iPhone 13", price:86800},
+  {id:6, tag:"iPhone", name:"iPhone SE", price:57800},
+]
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />
+    element: <Root  />
+  },
+  {
+    path: "products/:productId",
+    element: <div>Products details</div>
+  },
+  {
+    path: "carts",
+    element: <div>Carts</div>
+  },
+  {
+    path: "favorites",
+    element: <div>Favorite Products</div>
   },
 ]);
 
