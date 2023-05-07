0b6d9cc722bf8081305cf621890ac247a726f265
setodeve <keinick11@outlook.com>
2023-05-07 21:09:30 +0900

    Edit Guess Number

--- Recursion/Python/guessnumber.py
+++ Recursion/Python/guessnumber.py
@@ -1,2 +1,13 @@
 import random
 
+maxNumber = int(input('Enter Max Number : '))
+minNumber = int(input('Enter Min Number : '))
+randomNuber = random.randint(minNumber, maxNumber)
+print(f'Guess the Number between {maxNumber} and {minNumber} ')
+while(1):
+  guessNumber = int(input())
+  if(randomNuber == guessNumber):
+    print("Numbers Matched. Congratulation!!")
+    break
+
+
