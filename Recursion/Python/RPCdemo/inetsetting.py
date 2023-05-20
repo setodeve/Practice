0db1c5348c312e830c93c37e97f139928bd389ac
setodeve <keinick11@outlook.com>
2023-05-21 08:18:18 +0900

    Edit InetSetting Class

--- Recursion/Python/RPCdemo/inetsetting.py
+++ Recursion/Python/RPCdemo/inetsetting.py
@@ -4,5 +4,12 @@ import os
 class InetSetting:
     def __init__(self,address) -> None:
         self.address = address
+        self.sock = socket.socket(socket.AF_UNIX,socket.SOCK_DGRAM)
 
-    
+    def address_bind(self):
+        sock = self.sock
+        sock.bind(self.address)
+
+    def address_unlink(self):
+        sock = self.sock
+        os.unlink(self.address)
