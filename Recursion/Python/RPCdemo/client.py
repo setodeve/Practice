f10c08de7ae2055c6ae4ea4bb1bf9b07fa09dbf8
setodeve <keinick11@outlook.com>
2023-05-24 23:06:28 +0900

    deal with response

--- Recursion/Python/RPCdemo/client.py
+++ Recursion/Python/RPCdemo/client.py
@@ -1,21 +1,29 @@
+import sys
+sys.dont_write_bytecode = True
 from inetsetting import InetSetting
+import json
 
 class Client(InetSetting):
     def __init__(self) -> None:
         super().__init__('127.0.0.2')
         self.server_address = '127.0.0.3'
+        self.data = {
+          "method": "floor",
+          "params": 1.5
+        }
 
     def data_receive(self):
-      message = input()
+      message = self.data
       sock = self.sock
       server_address = self.server_address
-      
+
       try:
         print('sending {!r}'.format(message))
-        sent = sock.sendto(message.encode(), server_address)
+        sent = sock.sendto(json.dumps(message).encode(), server_address)
         print('waiting to receive')
         data, server = sock.recvfrom(4096)
-        print('received {!r}'.format(data.decode('utf-8')))
+        jsondata = json.loads(data.decode('utf-8'))
+        print('received {!r}'.format(jsondata))
 
       finally:
         print('closing socket')
--- Recursion/Python/RPCdemo/server.py
+++ Recursion/Python/RPCdemo/server.py
@@ -1,5 +1,8 @@
+import sys
+sys.dont_write_bytecode = True
 from inetsetting import InetSetting
-
+import json
+import math
 class Server(InetSetting):
     def __init__(self) -> None:
         super().__init__('127.0.0.3')
@@ -9,14 +12,35 @@ class Server(InetSetting):
       
       while True:
         print('\nwaiting to receive message')
-        data, address = sock.recvfrom(4096)
-
-        print('received {} bytes from {}'.format(len(data), address))
-        print('{!r}'.format(data.decode('utf-8')))
-
-        if data:
-            sent = sock.sendto(data, address)
+        recvjsondata, address = sock.recvfrom(4096)
+        recvdata = json.loads(recvjsondata.decode('utf-8'))
+        
+        print('received {} from {}'.format(len(recvdata), address))
+        print('{!r}'.format(recvdata))
+        data = {
+          "results":int(math.floor(recvdata["params"])),
+          "type":"int"
+        }
+        sendjsondata = json.dumps(data).encode()
+        # sendjsondata = self.select_methods(recvdata["method"],recvdata["params"])
+        senddata = json.loads(sendjsondata.decode('utf-8'))
+        
+        if senddata:
+            sent = sock.sendto(sendjsondata, address)
             print('sent {} bytes back to {}'.format(sent, address))
+
+    # def floor_number(num):
+    #    data = {
+    #       "results":int(math.floor(num)),
+    #       "type":"int"
+    #    }
+    #    return json.dumps(data).encode()
+    
+    # def select_methods(self,str,params):
+    #    if str=="floor":
+    #       return self.floor_number(params)
+    #    else:
+    #       return json.dumps({}).encode()
 
 server = Server()
 server.address_unlink()
