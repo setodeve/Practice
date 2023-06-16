0e8995e6c410fa9f1312d33c1119fe583814e1f3
setodeve <keinick11@outlook.com>
2023-06-16 23:02:32 +0900

    add getAllPostIds

--- NextJS/nextjs-blog/lib/posts.js
+++ NextJS/nextjs-blog/lib/posts.js
@@ -28,3 +28,15 @@ export function getSortedPostData() {
     }
   });
 }
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postDirectory)
+
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/,''),
+      },
+    };
+  });
+}
