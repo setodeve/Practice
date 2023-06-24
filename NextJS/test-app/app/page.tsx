151b12d910f699845fd53d7a35deffcf2172013b
setodeve <keinick11@outlook.com>
2023-06-24 22:32:56 +0900

    add github token

--- NextJS/test-app/app/page.tsx
+++ NextJS/test-app/app/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { testState } from '@/components/stroe'
+import 'dotenv/config'
 
 export default function Home() {
   const [text,setText] = useRecoilState(testState)
+  // const test = process.env.GITHUB_TOKEN
   return (
     <main>
       <h1>{text.name}</h1>
--- NextJS/test-app/components/stroe.ts
+++ NextJS/test-app/components/stroe.ts
@@ -1,10 +1,19 @@
 "use client"
 import { atom } from "recoil";
+import 'dotenv/config'
 
 export const testState = atom({
   key: "textState",
   default: {
     id:null,
     name:"setodeve"
+  }
+})
+
+export const githubState = atom({
+  key: "gitState",
+  default: {
+    token:process.env.GITHUB_TOKEN,
+    url:process.env.GITHUB_BASEURL
   }
 })
