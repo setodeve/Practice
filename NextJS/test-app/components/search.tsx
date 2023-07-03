6570dd255179c3bcebdcffa8162d9621aa79da29
setodeve <keinick11@outlook.com>
2023-07-04 05:42:07 +0900

    add search function

--- NextJS/test-app/components/search.tsx
+++ NextJS/test-app/components/search.tsx
@@ -1,12 +1,27 @@
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { searchwordState } from '@/components/stroe'
+import { FormEvent } from 'react'
+import type { InferGetStaticPropsType, GetStaticProps } from 'next'
+import 'dotenv/config'
 
 export default function Search(){
   const [searchword,setSearchword] = useRecoilState(searchwordState)
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+    const res = await fetch("https://api.github.com/search/repositories?q="+`${event.target.word.value}`,{
+      method: 'GET',
+      headers:{
+        "Accept": "application/vnd.github+json",
+        "Authorization": "Bearer "+`${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+    const repo = await res.json()
+    setSearchword(repo)
+  }
   return (
-    <form action="/api/form" method="post">
-      <label htmlFor="word">word</label>
-      <input type="text" id="word" name="word"  required />
+    <form onSubmit={handleSubmit}>
+      <input type="text" id="word" name="word" placeholder='word for repository search' required />
       <button type="submit">Submit</button>
     </form>
   )
--- NextJS/test-app/pages/lists.tsx
+++ NextJS/test-app/pages/lists.tsx
@@ -1,43 +1,52 @@
 import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 import 'dotenv/config'
-
-type Repo = {
-  name: string
-  stargazers_count: number
-}
+import Search from '@/components/search'
+import { searchwordState } from '@/components/stroe'
+// type Repo = {
+//   name: string
+//   stargazers_count: number
+// }
  
-export const getStaticProps: GetStaticProps<{
-  repo: Repo
-}> = async () => {
-  const res = await fetch('https://api.github.com/search/repositories?q=anycloud',{
-    method: 'GET',
-    headers:{
-      "Accept": "application/vnd.github+json",
-      "Authorization": "Bearer "+`${process.env.GITHUB_TOKEN}`,
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  })
-  const repo = await res.json()
-  return { props: { repo } }
-}
+// export const getStaticProps: GetStaticProps<{
+//   repo: Repo
+// }> = async () => {
+//   const res = await fetch('https://api.github.com/search/repositories?q=anycloud',{
+//     method: 'GET',
+//     headers:{
+//       "Accept": "application/vnd.github+json",
+//       "Authorization": "Bearer "+`${process.env.GITHUB_TOKEN}`,
+//       'X-GitHub-Api-Version': '2022-11-28'
+//     }
+//   })
+//   const repo = await res.json()
+//   return { props: { repo } }
+// }
  
-export default function List({
-  repo,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
-  console.log(repo)
-  const items = repo.items
+export default function List(){
+  // console.log(repo)
+  // const items = repo.items
   return (
     <>
-      <ul>
-        {/* {items.map(({item}) => (
-            <li>{item}<li/>
-        ))} */}
-          {items.map(({id,name}) => (
-            <li key={id}>
-              {name}
-            </li>
-          ))}
-      </ul>
+      <Search></Search>
     </>
   );
 }
+
+// export default function List({
+//   repo,
+// }: InferGetStaticPropsType<typeof getStaticProps>) {
+//   console.log(repo)
+//   const items = repo.items
+//   return (
+//     <>
+//       <></>
+//       <ul>
+//           {items.map(({id,name}) => (
+//             <li key={id}>
+//               {name}
+//             </li>
+//           ))}
+//       </ul>
+//     </>
+//   );
+// }
