ff7aaa52d8b6bf745e17a6675c9f33c3ce199303
setodeve <keinick11@outlook.com>
2023-07-04 21:18:11 +0900

    delete uneeded state

--- NextJS/test-app/app/page.tsx
+++ NextJS/test-app/app/page.tsx
@@ -1,13 +1,10 @@
 "use client"
-import { useRecoilState, useRecoilValue } from 'recoil'
-import { testState } from '@/components/stroe'
 import 'dotenv/config'
 
 export default function Home() {
-  const [text,setText] = useRecoilState(testState)
   return (
     <main>
-      <h1>{text.name}</h1>
+      <div>Home</div>
     </main>
   )
 }
--- NextJS/test-app/components/stroe.ts
+++ NextJS/test-app/components/stroe.ts
@@ -1,14 +1,6 @@
 "use client"
 import { atom } from "recoil";
 
-export const testState = atom({
-  key: "textState",
-  default: {
-    id:null,
-    name:"setodeve"
-  }
-})
-
 export const searchwordState = atom({
   key: "searchwordState",
   default: []
