f4bc440a9fd14d0f1f991b85b4b0f9a573cad737
setodeve <keinick11@outlook.com>
2023-07-17 11:50:15 +0900

    change header for getting usetinfo

--- NextJS/test-app/components/header.tsx
+++ NextJS/test-app/components/header.tsx
@@ -1,12 +1,66 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from 'next/image'
 import { useRecoilState } from 'recoil'
-import { userInfoState } from '@/components/stroe'
+import { useEffect } from 'react'
+import { userInfoState,userRepoState,userSessionState } from '@/components/stroe'
+import { getUserData } from './lib'
+import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 
-export default function Head(){
+type Repo = {
+  name: string
+  stargazers_count: number
+}
+
+export const getStaticProps: GetStaticProps<{
+  repo: Repo
+}> = async () => {
+  const res = await fetch("https://api.github.com/users/setokei",{
+    method: 'GET',
+    headers:{
+      "Accept": "application/vnd.github+json",
+      "Authorization": "Bearer "+`${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+      'X-GitHub-Api-Version': '2022-11-28'
+    }})
+    // console.log(res)
+    const repo = await res.json()
+  // console.log(repo)
+  return { props: { repo } }
+}
+
+export default function Head({
+  repo,
+}: InferGetStaticPropsType<typeof getStaticProps>){
   const [userInfo,setUserInfo] = useRecoilState(userInfoState)
+  const [userRepo,setUserRepo] = useRecoilState(userRepoState)
+  const [userSession,setUserSession] = useRecoilState(userSessionState)
   const { data: session, status } = useSession();
   const loading = status === "loading";
+  // console.log(repo)
+  // console.log("pass")
+  useEffect(() => {
+      // if(session) setUserSession(session)
+      // if(userSession.name!==undefined){
+      //   getUserData(`users/${userSession.user.name}`)
+      //   .then(data => setUserInfo(data.json()))
+      // }
+        // setUserRepo( await getUserData(`users/${session.user.name}/repos`))
+        if(session && Object.keys(userSession).length==0){
+          fetch("https://api.github.com/users/"+`${session.user.name}`,{
+            method: 'GET',
+            headers:{
+              "Accept": "application/vnd.github+json",
+              "Authorization": "Bearer "+`${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+              'X-GitHub-Api-Version': '2022-11-28'
+            }
+            })
+            .then((res) => res.json())
+            .then((data) => {
+              setUserInfo(data)
+            })
+        }
+    // console.log(userSession)
+    console.log(userInfo)
+  }, [session])
   return (
     <header>
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
