7ecc73d7a53d62072657db1533561ed170518cbf
setodeve <keinick11@outlook.com>
2023-07-21 07:57:48 +0900

    change repos component

--- NextJS/test-app/components/repos.tsx
+++ NextJS/test-app/components/repos.tsx
@@ -1,3 +1,27 @@
-export default function Repos(data:any[]){
+import { userRepoState } from '@/components/stroe'
+import { useRecoilState } from 'recoil'
 
+async function getData(name:string){
+  return await fetch("https://api.github.com/users/"+`${name}`)
+              .then((res) => res.json())
+              .then((data) => {
+                return data
+              })
+}
+
+export default function Repos(data:any[]){
+  const [userRepo,setUserRepo] = useRecoilState(userRepoState)
+
+  if(data&& Object.keys(userRepo).length==0&& Object.keys(userInfo).length==0) setUserRepo(await getData(`${data.name}`+"/repos"))
+
+  return (
+   <>
+    {Object.keys(userRepo).length==0 ? (
+      <>no repository</>
+    ): (
+      <>
+      </>
+    )}
+   </>  
+  )
 }
