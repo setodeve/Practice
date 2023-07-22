46b45b178b39c47e13a2bbbe6d23dea03d3577b0
setodeve <keinick11@outlook.com>
2023-07-22 19:25:34 +0900

    change repos component

--- NextJS/test-app/components/repos.tsx
+++ NextJS/test-app/components/repos.tsx
@@ -1,27 +1,22 @@
-import { userRepoState } from '@/components/stroe'
-import { useRecoilState } from 'recoil'
+// import { userRepoState } from '@/components/stroe'
+// import { useRecoilState } from 'recoil'
 
-async function getData(name:string){
-  return await fetch("https://api.github.com/users/"+`${name}`)
-              .then((res) => res.json())
-              .then((data) => {
-                return data
-              })
-}
+// async function getData(name:string){
+//   return await fetch("https://api.github.com/users/"+`${name}`)
+//               .then((res) => res.json())
+//               .then((data) => {
+//                 return data
+//               })
+// }
 
-export default async function Repos(data:any){
-  const [userRepo,setUserRepo] = useRecoilState(userRepoState)
-  
-  if(data&& Object.keys(userRepo).length==0) setUserRepo(await getData(`${data.name}`+"/repos"))
+export default function Repos(data:any){
+  // const [userRepo,setUserRepo] = useRecoilState(userRepoState)
+
+  // if(data&& Object.keys(userRepo).length==0) setUserRepo(await getData(`${data.name}`+"/repos"))
 
   return (
    <>
-    {Object.keys(userRepo).length==0 ? (
-      <>no repository</>
-    ): (
-      <>
-      </>
-    )}
+    <h1>some repositories</h1>
    </>  
   )
 }
--- NextJS/test-app/components/user.tsx
+++ NextJS/test-app/components/user.tsx
@@ -1,5 +1,7 @@
 import { userInfoState,userRepoState } from '@/components/stroe'
 import { useRecoilState } from 'recoil'
+import Repos from "@/components/repos"
+import Pagination from './pagination'
 
 async function getData(name:string){
   return await fetch("https://api.github.com/users/"+`${name}`)
@@ -11,9 +13,9 @@ async function getData(name:string){
 
 export default async function User(data:any){
   const [userInfo,setUserInfo] = useRecoilState(userInfoState)
-  // const [userRepo,setUserRepo] = useRecoilState(userRepoState)
+  const [userRepo,setUserRepo] = useRecoilState(userRepoState)
 
-  // if(data&& Object.keys(userRepo).length==0&& Object.keys(userInfo).length==0) setUserRepo(await getData(`${data.name}`+"/repos"))
+  if(data&& Object.keys(userRepo).length==0&& Object.keys(userInfo).length==0) setUserRepo(await getData(`${data.name}`+"/repos"))
   if(data&& Object.keys(userInfo).length==0) setUserInfo(await getData(data.name))
 
   return (
@@ -25,16 +27,16 @@ export default async function User(data:any){
         <div>
           <h1>{userInfo.login}</h1>
         </div>
-        {/* {
+        {
           Object.keys(userRepo).length==0 ? (
             <>
               <h1>no repository</h1>
             </>
           ):(
             <>
-              <h1>some repositories</h1>
+              <Repos data={userRepo}/>
             </>
-          )} */}
+          )}
       </>
     )}
    </>  
