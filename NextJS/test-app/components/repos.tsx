5497bfd9eab47f1be26135342cfd241b041c9554
setodeve <keinick11@outlook.com>
2023-07-26 07:22:14 +0900

    delete comment

--- NextJS/test-app/components/repos.tsx
+++ NextJS/test-app/components/repos.tsx
@@ -1,19 +1,4 @@
-// import { userRepoState } from '@/components/stroe'
-// import { useRecoilState } from 'recoil'
-
-// async function getData(name:string){
-//   return await fetch("https://api.github.com/users/"+`${name}`)
-//               .then((res) => res.json())
-//               .then((data) => {
-//                 return data
-//               })
-// }
-
 export default function Repos(data:any){
-  // const [userRepo,setUserRepo] = useRecoilState(userRepoState)
-
-  // if(data&& Object.keys(userRepo).length==0) setUserRepo(await getData(`${data.name}`+"/repos"))
-
   return (
    <>
     <h1>some repositories</h1>
