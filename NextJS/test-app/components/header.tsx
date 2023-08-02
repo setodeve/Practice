34f0f0e62901eba8edf46e48d37dd0e69a7507b0
setodeve <keinick11@outlook.com>
2023-08-02 20:31:42 +0900

    delete comment

--- NextJS/test-app/components/header.tsx
+++ NextJS/test-app/components/header.tsx
@@ -1,46 +1,11 @@
-// import { userInfoState, userRepoState, currentPageState } from '@/components/stroe'
-// import { useRecoilState } from 'recoil'
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Head() {
-  // const [userInfo,setUserInfo] = useRecoilState(userInfoState)
-  // const [userRepo,setUserRepo] = useRecoilState(userRepoState)
-  // const [userSession,setUserSession] = useRecoilState(userSessionState)
   const { data: session, status } = useSession();
   const loading = status === "loading";
 
-  // useEffect(() => {
-    // if(session&&Object.keys(userInfo).length==0) {
-    //   getData()
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     setUserInfo(data)
-    //   })
-    // }
-      // if(session) setUserSession(session)
-      // if(userSession.name!==undefined){
-      //   getUserData(`users/${userSession.user.name}`)
-      //   .then(data => setUserInfo(data.json()))
-      // }
-        // setUserRepo( await getUserData(`users/${session.user.name}/repos`))
-        // if(session && Object.keys(userInfo).length==0){
-        //   fetch("https://api.github.com/users/"+`${session.user.name}`,{
-        //     method: 'GET',
-        //     headers:{
-        //       "Accept": "application/vnd.github+json",
-        //       "Authorization": "Bearer "+`${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
-        //       'X-GitHub-Api-Version': '2022-11-28'
-        //     }
-        //     })
-        //     .then((res) => res.json())
-        //     .then((data) => {
-        //       setUserInfo(data)
-        //     })
-        // }else{
-
-        // }
   return (
     <header>
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
