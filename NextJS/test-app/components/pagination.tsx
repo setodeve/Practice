6d5e900a0e5dfb534052673eeef33bd90da9f8fd
setodeve <keinick11@outlook.com>
2023-08-04 18:40:29 +0900

    delete comment

--- NextJS/test-app/components/pagination.tsx
+++ NextJS/test-app/components/pagination.tsx
@@ -1,24 +1,3 @@
-// import Link from 'next/link';
-// import { useRecoilState } from 'recoil'
-// import { searchresultState } from '@/components/stroe'
-// export default function Pagination({ totalCount }:number){
-//   const [searchresult,setSearchresult] = useRecoilState(searchresultState)
-//   const PER_PAGE = 5;
-
-//   const range = (start :number, end :number) =>
-//         [...Array(end - start + 1)].map((_, i) => start + i)
-
-//   return (
-//     <ul>
-//       {range(1, Math.ceil(totalCount / PER_PAGE)).map((number :number, index :number) => (
-//         <li key={index}>
-//           <Link href={`/blog/page/${number}`}>{number}</Link>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
 const Pagination = ({ items, pageSize, currentPage, onPageChange }) => {
   const pagesCount = Math.ceil(items / pageSize); // 100/10
  
