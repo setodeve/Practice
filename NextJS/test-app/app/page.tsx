744c8b657c612a59475256228319f49686402782
setodeve <keinick11@outlook.com>
2023-08-10 21:11:36 +0900

    delete colon

--- NextJS/test-app/app/page.tsx
+++ NextJS/test-app/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Search from '@/components/search'
 import List  from '@/components/list'
-import Head from '@/components/header';
+import Head from '@/components/header'
 
 export default function Home(){
   return (
