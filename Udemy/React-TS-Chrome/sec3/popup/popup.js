4313db088f5632e10109dab74419ed725cd6e6d2
setodeve <keinick11@outlook.com>
2023-08-26 15:28:54 +0900

    change popup.js

--- Udemy/React-TS-Chrome/sec3/popup/popup.js
+++ Udemy/React-TS-Chrome/sec3/popup/popup.js
@@ -1,13 +1,18 @@
+const tasks = []
 const addTaskBtn = document.getElementById("add-btn");
 addTaskBtn.addEventListener("click", ()=> addTask());
 
 function addTask(){
+  const taskNum = tasks.length
+  tasks.push("")
+
   const taskRow = document.createElement("div");
   const text = document.createElement("input");
   text.type = "text";
   text.placeholder = "Enter task......";
   text.addEventListener("change",()=>{
-    
+    tasks[taskNum] = text.value
+    console.log(tasks)
   })
   const deleteBtn = document.createElement("input");
   deleteBtn.type = "button";
