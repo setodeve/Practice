ffc5a7452534102f5338fa5bc0c36869b2c6cd0f
setodeve <keinick11@outlook.com>
2023-09-01 06:59:53 +0900

    change timer setting

--- Udemy/React-TS-Chrome/sec3/background.js
+++ Udemy/React-TS-Chrome/sec3/background.js
@@ -1,3 +1,23 @@
 chrome.alarms.create("pomodoroTimer", {
   periodInMinutes: 1/60,
 })
+
+chrome.alarms.onAlarm.addListener((alarm)=>{
+  if(alarm.name=="pomodoroTimer"){
+    chrome.storage.local.get(["get","isRunning"],(res)=>{
+      if(res.isRunning){
+        let timer = res.timer + 1
+        chrome.storage.local.set({
+          timer,
+        })
+      }
+    })
+  }
+})
+
+chrome.storage.local.get(["timer", "isRunning"],(res)=>{
+  chrome.storage.local.set({
+    timer: "timer" in res ? res.timer : 0,
+    isRunning: "isRunning" in res ? res.isRunning : false
+  })
+})
