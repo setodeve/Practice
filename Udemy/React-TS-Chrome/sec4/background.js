3076731b1864c08be58f02ff56e1a39752fa5a99
setodeve <keinick11@outlook.com>
2023-09-17 11:15:20 +0900

    change file

--- Udemy/React-TS-Chrome/sec4/background.js
+++ Udemy/React-TS-Chrome/sec4/background.js
@@ -8,10 +8,15 @@ chrome.runtime.onInstalled.addListener((details) => {
     console.log(event)
   })
   chrome.contextMenus.create({
-    title: "Test Menu",
-    id: "contextMenu1",
+    title: "Test Menu2",
+    id: "contextMenu2",
     contexts: ["page","selection"]
   })
+  chrome.contextMenus.create({
+    title: "Test Menu3",
+    id: "contextMenu3",
+    contexts: ["page","selection"]
+  })  
 })
 
 console.log("running")
