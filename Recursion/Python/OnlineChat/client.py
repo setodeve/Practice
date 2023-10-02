7c25fbfa0aa81711e7719753fd3e1c566ee4730e
setodeve <keinick11@outlook.com>
2023-10-03 00:18:40 +0900

    add client and server

--- Recursion/Python/OnlineChat/client.py
+++ Recursion/Python/OnlineChat/client.py
@@ -1,6 +1,6 @@
 from inetsetting import InetSetting
 import json
-
+from faker import Faker
 class Client(InetSetting):
     def __init__(self) -> None:
         super().__init__('127.0.0.2')
@@ -9,3 +9,23 @@ class Client(InetSetting):
           "method": "valid",
           "params": {"str1": "abc","str2": "cba"}
         }
+
+    def exectsend(self):
+      fake = Faker()
+      message = fake.name
+
+      self.sock.bind(self.address)
+      try:
+        print('sending {!r}'.format(message))
+        sent = self.sock.sendto(message.encode(), self.server_address)
+
+        print('waiting to receive')
+        data, server = self.sock.recvfrom(4096)
+        print('received {!r}'.format(data.decode('utf-8')))
+
+      finally:
+        print('closing socket')
+        self.sock.close()
+
+client = Client()
+client.exectsend
--- Recursion/Python/OnlineChat/server.py
+++ Recursion/Python/OnlineChat/server.py
@@ -3,3 +3,21 @@ from inetsetting import InetSetting
 class Server(InetSetting):
     def __init__(self) -> None:
         super().__init__('127.0.0.3')
+
+    def exectreciecve(self):
+        print('starting up on {}'.format(self.address))
+        self.sock.bind(self.address)
+
+        while True:
+            print('\nwaiting to receive message')
+            data, address = self.sock.recvfrom(4096)
+
+            print('received {} bytes from {}'.format(len(data), address))
+            print('{!r}'.format(data.decode('utf-8')))
+
+            if data:
+                sent = self.sock.sendto(data, address)
+                print('sent {} bytes back to {}'.format(sent, address))
+
+server = Server()
+server.exectreciecve()
