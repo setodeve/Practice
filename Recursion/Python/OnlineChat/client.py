d853adaff02821faa47e9014feaab171a3e1efc5
setodeve <keinick11@outlook.com>
2023-10-05 20:23:40 +0900

    edit client

--- Recursion/Python/OnlineChat/client.py
+++ Recursion/Python/OnlineChat/client.py
@@ -1,31 +1,38 @@
 from inetsetting import InetSetting
 import json
-from faker import Faker
+# from faker import Faker
 class Client(InetSetting):
     def __init__(self) -> None:
-        super().__init__('127.0.0.2')
+        super().__init__('127.0.0.2',1234)
         self.server_address = '127.0.0.3'
         self.data = {
           "method": "valid",
           "params": {"str1": "abc","str2": "cba"}
         }
 
-    def exectsend(self):
-      fake = Faker()
-      message = fake.name
+    def exectsend(self)-> None:
+      # fake = Faker()
+      # message = fake.name
 
-      self.sock.bind(self.address)
-      try:
-        print('sending {!r}'.format(message))
-        sent = self.sock.sendto(message.encode(), self.server_address)
+      # self.address_bind()
+      while True:
+        try:
+          print('Input any messages, Type [end] to exit')
+          message = input()
+          if message != 'end':
+            print('sending {!r}'.format(message))
+            sent = self.sock.sendto(message.encode(), (self.server_address,self.port))
 
-        print('waiting to receive')
-        data, server = self.sock.recvfrom(4096)
-        print('received {!r}'.format(data.decode('utf-8')))
+            print('waiting to receive')
+            data, server = self.sock.recvfrom(4096)
+            print('received {!r}'.format(data.decode('utf-8')))
+          else:
+            print('closing socket')
+            self.sock.close()
 
-      finally:
-        print('closing socket')
-        self.sock.close()
+        finally:
+          print('closing socket')
+          self.sock.close()
 
 client = Client()
-client.exectsend
+client.exectsend()
