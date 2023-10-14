fe6e8a35f77b8418eb1e0270b40ffb197349b7ad
setodeve <keinick11@outlook.com>
2023-10-14 11:28:20 +0900

    delete unneeded variable

--- Recursion/Python/OnlineChat/client.py
+++ Recursion/Python/OnlineChat/client.py
@@ -5,10 +5,10 @@ class Client(InetSetting):
     def __init__(self) -> None:
         super().__init__('127.0.0.2',1234)
         self.server_address = '127.0.0.3'
-        self.data = {
-          "method": "valid",
-          "params": {"str1": "abc","str2": "cba"}
-        }
+        # self.data = {
+        #   "method": "valid",
+        #   "params": {"str1": "abc","str2": "cba"}
+        # }
 
     def exectsend(self)-> None:
       # self.address_bind()
