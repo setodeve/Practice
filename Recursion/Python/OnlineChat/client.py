2658a6847e3e52afd1a1254a8cb0abf2ec2bfde7
setodeve <keinick11@outlook.com>
2023-10-14 12:27:46 +0900

    edit client

--- Recursion/Python/OnlineChat/client.py
+++ Recursion/Python/OnlineChat/client.py
@@ -3,12 +3,8 @@ import json
 
 class Client(InetSetting):
     def __init__(self) -> None:
-        super().__init__('127.0.0.2',1234)
+        super().__init__('127.0.0.1',8890)
         self.server_address = '127.0.0.3'
-        # self.data = {
-        #   "method": "valid",
-        #   "params": {"str1": "abc","str2": "cba"}
-        # }
 
     def exectsend(self)-> None:
       # self.address_bind()
@@ -32,4 +28,33 @@ class Client(InetSetting):
           self.sock.close()
 
 client = Client()
-client.exectsend()
+
+# ①ソケットを作成する
+sock = client.sock
+
+while True:
+    try:
+        # ②messageを送信する
+        print('Input any messages, Type [end] to exit')
+        message = input()
+        if message != 'end':
+            send_len = sock.sendto(message.encode('utf-8'), client.setinfo)
+            # ※sendtoメソッドはkeyword arguments(address=serv_addressのような形式)を受け付けないので注意
+
+            # ③Serverからのmessageを受付開始
+            print('Waiting response from Server')
+            rx_meesage, addr = sock.recvfrom(client.size)
+            print(f"[Server]: {rx_meesage.decode(encoding='utf-8')}")
+
+        else:
+            print('closing socket')
+            sock.close()
+            print('done')
+            break
+
+    except KeyboardInterrupt:
+        print('closing socket')
+        sock.close()
+        print('done')
+        break
+
