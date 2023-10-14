ef97f0fbc41324b6984dc68fe6d1bb17f70109ab
setodeve <keinick11@outlook.com>
2023-10-14 16:16:38 +0900

    edit client

--- Recursion/Python/OnlineChat/client.py
+++ Recursion/Python/OnlineChat/client.py
@@ -1,5 +1,4 @@
 from inetsetting import InetSetting
-import json
 
 class Client(InetSetting):
     def __init__(self) -> None:
@@ -7,54 +6,32 @@ class Client(InetSetting):
         self.server_address = '127.0.0.3'
 
     def exectsend(self)-> None:
-      # self.address_bind()
       while True:
-        try:
-          print('Input any messages, Type [end] to exit')
-          message = input()
-          if message != 'end':
-            print('sending {!r}'.format(message))
-            sent = self.sock.sendto(message.encode(), (self.server_address,self.port))
+          try:
+              # ②messageを送信する
+              print('Input any messages, Type [end] to exit')
+              message = input()
+              if message != 'end':
+                  send_len = self.sock.sendto(message.encode('utf-8'), self.setinfo)
+                  # ※sendtoメソッドはkeyword arguments(address=serv_addressのような形式)を受け付けないので注意
 
-            print('waiting to receive')
-            data, server = self.sock.recvfrom(4096)
-            print('received {!r}'.format(data.decode('utf-8')))
-          else:
-            print('closing socket')
-            self.sock.close()
+                  # ③Serverからのmessageを受付開始
+                  print('Waiting response from Server')
+                  rx_meesage, addr = self.sock.recvfrom(self.size)
+                  print(f"[Server]: {rx_meesage.decode(encoding='utf-8')}")
 
-        finally:
-          print('closing socket')
-          self.sock.close()
+              else:
+                  print('closing socket')
+                  self.sock.close()
+                  print('done')
+                  break
+
+          except KeyboardInterrupt:
+              print('closing socket')
+              self.sock.close()
+              print('done')
+              break
 
 client = Client()
-
-# ①ソケットを作成する
-sock = client.sock
-
-while True:
-    try:
-        # ②messageを送信する
-        print('Input any messages, Type [end] to exit')
-        message = input()
-        if message != 'end':
-            send_len = sock.sendto(message.encode('utf-8'), client.setinfo)
-            # ※sendtoメソッドはkeyword arguments(address=serv_addressのような形式)を受け付けないので注意
-
-            # ③Serverからのmessageを受付開始
-            print('Waiting response from Server')
-            rx_meesage, addr = sock.recvfrom(client.size)
-            print(f"[Server]: {rx_meesage.decode(encoding='utf-8')}")
-
-        else:
-            print('closing socket')
-            sock.close()
-            print('done')
-            break
-
-    except KeyboardInterrupt:
-        print('closing socket')
-        sock.close()
-        print('done')
-        break
+client.exectsend()
 
