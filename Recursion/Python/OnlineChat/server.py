6966d5342a8f6a9857e02e26e1c3fdab038641ca
setodeve <keinick11@outlook.com>
2023-10-14 12:24:54 +0900

    edit server

--- Recursion/Python/OnlineChat/server.py
+++ Recursion/Python/OnlineChat/server.py
@@ -5,9 +5,23 @@ import time
 class Server(InetSetting):
     def __init__(self) -> None:
         super().__init__('127.0.0.1',8890)
+        
+    def exectreciecve(self)-> None:
+        print('starting up on {}'.format(self.address))
+        self.address_bind()
+
+        while True:
+            print('\nwaiting to receive message')
+            data, address = self.sock.recvfrom(4096)
+
+            print('received {} bytes from {}'.format(len(data), address))
+            print('{!r}'.format(data.decode('utf-8')))
+
+            if data:
+                sent = self.sock.sendto(data, address)
+                print('sent {} bytes back to {}'.format(sent, address))
 
 server = Server()
-
 # ①ソケットを作成する
 sock = server.sock
 print('create socket')
