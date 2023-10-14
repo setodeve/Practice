9494e7d0a9d45885d34e356f885e52f78b934c7f
setodeve <keinick11@outlook.com>
2023-10-14 19:49:34 +0900

    edit server

--- Recursion/Python/OnlineChat/server.py
+++ Recursion/Python/OnlineChat/server.py
@@ -6,44 +6,27 @@ class Server(InetSetting):
         super().__init__('127.0.0.1',8890)
 
     def exectreciecve(self)-> None:
-        print('starting up on {}'.format(self.address))
-        self.address_bind()
+        self.sock.bind(self.setinfo)
 
         while True:
-            print('\nwaiting to receive message')
-            data, address = self.sock.recvfrom(4096)
+            try :
+                # ③Clientからのmessageの受付開始
+                print('Waiting message')
+                message, cli_addr = self.sock.recvfrom(self.size)
+                message = message.decode(encoding='utf-8')
+                print(f'Received message is [{message}]')
 
-            print('received {} bytes from {}'.format(len(data), address))
-            print('{!r}'.format(data.decode('utf-8')))
+                # Clientが受信待ちになるまで待つため
+                time.sleep(1)
 
-            if data:
-                sent = self.sock.sendto(data, address)
-                print('sent {} bytes back to {}'.format(sent, address))
+                # ④Clientへ受信完了messageを送信
+                print('Send response to Client')
+                self.sock.sendto('Success to receive message'.encode(encoding='utf-8'), cli_addr)
+
+            except KeyboardInterrupt:
+                print ('\n . . .\n')
+                self.sock.close()
+                break
 
 server = Server()
-# ①ソケットを作成する
-sock = server.sock
-print('create socket')
-
-# ②自ホストで使用するIPアドレスとポート番号を指定
-sock.bind(server.setinfo)
-
-while True:
-    try :
-        # ③Clientからのmessageの受付開始
-        print('Waiting message')
-        message, cli_addr = sock.recvfrom(server.size)
-        message = message.decode(encoding='utf-8')
-        print(f'Received message is [{message}]')
-
-        # Clientが受信待ちになるまで待つため
-        time.sleep(1)
-
-        # ④Clientへ受信完了messageを送信
-        print('Send response to Client')
-        sock.sendto('Success to receive message'.encode(encoding='utf-8'), cli_addr)
-
-    except KeyboardInterrupt:
-        print ('\n . . .\n')
-        sock.close()
-        break
+server.exectreciecve()
