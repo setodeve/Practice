e06f46dfeceb72e41a0e6618069362357a3b2bc0
setodeve <keinick11@outlook.com>
2023-10-14 16:33:05 +0900

    edit inetsetting

--- Recursion/Python/OnlineChat/inetsetting.py
+++ Recursion/Python/OnlineChat/inetsetting.py
@@ -11,13 +11,13 @@ class InetSetting:
         self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         self.size = 1024
 
-    def address_bind(self):
-        # sock = self.sock
-        self.sock.bind(self.setinfo)
-        print('starting up on {}'.format(self.address))
+    # def address_bind(self):
+    #     # sock = self.sock
+    #     self.sock.bind(self.setinfo)
+    #     print('starting up on {}'.format(self.address))
 
-    def address_unlink(self):
-        try:
-            os.unlink(self.address)
-        except FileNotFoundError:
-            pass
+    # def address_unlink(self):
+    #     try:
+    #         os.unlink(self.address)
+    #     except FileNotFoundError:
+    #         pass
