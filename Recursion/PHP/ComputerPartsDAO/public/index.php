20dc8688d8197d19e203a73591970de2a5ba632b
setodeve <keinick11@outlook.com>
2024-02-27 23:03:00 +0900

    edit ComputerPartsDAO

--- Recursion/PHP/ComputerPartsDAO/public/index.php
+++ Recursion/PHP/ComputerPartsDAO/public/index.php
@@ -1,33 +1,36 @@
 <?php
+set_include_path(get_include_path() . PATH_SEPARATOR . realpath(__DIR__ . '/..'));
 spl_autoload_extensions(".php");
 spl_autoload_register();
 
 $DEBUG = true;
 
-// ルートを読み込みます。
+if (preg_match('/\.(?:png|jpg|jpeg|gif|js|css|html)$/', $_SERVER["REQUEST_URI"])) {
+    return false;
+}
+
+// ルートを読み込みます
 $routes = include('Routing/routes.php');
 
-// リクエストURIを解析してパスだけを取得します。
+// リクエストURIからパスだけを解析して取得します
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $path = ltrim($path, '/');
 
-// ルートにパスが存在するかチェックする
+// パスがルートに存在するかチェックします
 if (isset($routes[$path])) {
-    // コールバックを呼び出してrendererを作成します。
+    // レンダラーを作成するコールバックを呼び出します
     $renderer = $routes[$path]();
 
     try{
-        // ヘッダーを設定します。
+        // ヘッダーを設定します
         foreach ($renderer->getFields() as $name => $value) {
-            // ヘッダーに対する単純な検証を実行します。
-            $sanitized_value = filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-
+            // ヘッダーに対して単純なバリデーションを実行します
+            $sanitized_value = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
             if ($sanitized_value && $sanitized_value === $value) {
-                header("Access-Control-Allow-Origin: *");
                 header("{$name}: {$sanitized_value}");
             } else {
-                // ヘッダー設定に失敗した場合、ログに記録するか処理します。
-                // エラー処理によっては、例外をスローするか、デフォルトのまま続行することもできます。
+                // ヘッダーの設定が失敗した場合のログを取るか、または処理します
+                // エラー処理に応じて、例外を投げるか、またはデフォルトで続行するかもしれません
                 http_response_code(500);
                 if($DEBUG) print("Failed setting header - original: '$value', sanitized: '$sanitized_value'");
                 exit;
@@ -42,7 +45,7 @@ if (isset($routes[$path])) {
         if($DEBUG) print($e->getMessage());
     }
 } else {
-    // マッチするルートがない場合、404エラーを表示します。
+    // ルートが一致しない場合は、404エラーを表示します
     http_response_code(404);
     echo "404 Not Found: The requested route was not found on this server.";
 }
